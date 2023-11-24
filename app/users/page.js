6f9bb32ab1160c174a29d56f86b7ca781d9474c6
baejoonam-macbook <baejoonam00@snu.ac.kr>
2023-11-24 20:12:31 +0900

    SSR

--- app/users/page.js
+++ app/users/page.js
@@ -1,8 +1,50 @@
-const Users = () => {
+const Users = async () => {
+	const users = [
+		{
+			name: '홍길동',
+			age: 20,
+		},
+		{
+			name: '춘향이',
+			age: 16,
+		},
+	]
+	console.log(users)
 	return (
 		<div>
 			<h3>Users</h3>
-			<p>Contents</p>
+			<hr className='d-block' />
+			<div>
+				<h4>Read</h4>
+				<table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Age</th>
+							<th>Modify</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((user, index) => (
+							<tr key={index}>
+								<td>{user.name}</td>
+								<td>{user.age}</td>
+								<td>
+									<button>Update</button>
+									<button>Delete</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+			<hr className='d-block' />
+			<div>
+				<h4>Create</h4>
+				<input type='text' placeholder='Name' />
+				<input type='text' placeholder='Age' />
+				<button>Create</button>
+			</div>
 		</div>
 	)
 }
