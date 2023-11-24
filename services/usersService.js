294d133ab5c48c509048b770d9e83bb9105cd6b8
baejoonam-macbook <baejoonam00@snu.ac.kr>
2023-11-24 20:54:14 +0900

    npm run build try catch로 감싸기

--- services/usersService.js
+++ services/usersService.js
@@ -1,15 +1,23 @@
 export const usersService = {
 	usersRead: async () => {
-		const res = await fetch('http://localhost:3000/api/users', {
-			cache: 'no-store', // 안하면 수정값이 안바뀌기도 함
-		})
-		return res.json()
+		try {
+			const res = await fetch('http://localhost:3000/api/users', {
+				cache: 'no-store', // 안하면 수정값이 안바뀌기도 함
+			})
+			return res.json()
+		} catch (error) {
+			return []
+		}
 	},
 	usersCreate: async user => {
-		const res = await fetch('http://localhost:3000/api/users', {
-			method: 'POST',
-			body: JSON.stringify(user),
-		})
-		return res.json()
+		try {
+			const res = await fetch('http://localhost:3000/api/users', {
+				method: 'POST',
+				body: JSON.stringify(user),
+			})
+			return res.json()
+		} catch (error) {
+			return []
+		}
 	},
 }
