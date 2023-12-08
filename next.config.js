33820e6a32bd6b2d3ca6abd2e850c3e3d29987a8
baejoonam-macbook <baejoonam00@snu.ac.kr>
2023-12-08 20:57:23 +0900

    proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/proxy/api/:path*',
+				destination: `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/:path*`,
+			},
+		]
+	},
+}
 
 module.exports = nextConfig
--- services/usersService.js
+++ services/usersService.js
@@ -2,7 +2,7 @@ export const usersService = {
 	usersRead: async () => {
 		try {
 			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/users`,
+				`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/users`, // 서버 사이드에서의 통신 요청은 전체 주소가 들어가야 함
 				{
 					cache: 'no-store', // 안하면 수정값이 안바뀌기도 함
 				}
@@ -14,36 +14,27 @@ export const usersService = {
 	},
 	usersCreate: async user => {
 		try {
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/users`,
-				{
-					method: 'POST',
-					body: JSON.stringify(user),
-				}
-			)
+			const res = await fetch(`/proxy/api/users`, {
+				method: 'POST',
+				body: JSON.stringify(user),
+			})
 			// return res.json()
 		} catch (error) {}
 	},
 	usersDelete: async userPk => {
 		try {
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/users/${userPk}`,
-				{
-					method: 'DELETE',
-				}
-			)
+			const res = await fetch(`/proxy/api/users/${userPk}`, {
+				method: 'DELETE',
+			})
 			return res.json()
 		} catch (error) {}
 	},
 	usersUpdate: async (userPk, user) => {
 		try {
-			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/users/${userPk}`,
-				{
-					method: 'PATCH',
-					body: JSON.stringify(user),
-				}
-			)
+			const res = await fetch(`/proxy/api/users/${userPk}`, {
+				method: 'PATCH',
+				body: JSON.stringify(user),
+			})
 			return res.json()
 		} catch (error) {}
 	},
